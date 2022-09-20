928d4b939fd9c25233ace2ffe6c3f3fc6b11f751
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2022-09-19 21:16:01 -0300

    Create endpoints

---
+++ springboot-api-Project/src/main/java/com/levi/levilog/SpringbootApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.levi.levilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiProjectApplication.class, args);
+	}
+
+}
